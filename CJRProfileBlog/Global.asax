5f2122309b2b2ed26c90b6f05bb3f9b085d4934f
ransomcjjr <ransomcjjr@gmail.com>
2016-10-20 17:17:01 -0400

    Revert "Revert "first commit""
    This reverts commit fa0d9a9704e2f501a4e4d5a2416857fc903fc3b9.

---
+++ CJRProfileBlog/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CJRProfileBlog.MvcApplication" Language="C#" %>
---
+++ CJRProfileBlog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CJRProfileBlog.Startup))]
+namespace CJRProfileBlog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
